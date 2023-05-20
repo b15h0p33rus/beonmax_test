496f798a030c291ba884e274d6ae267f0801de88
b15h0p <b15h0p@mail.ru>
2023-05-20 09:27:39 +0300

    sample massage

--- age.js
+++ age.js
@@ -1,1 +1,23 @@
-let age = parseInt(readLine(), 10);
+variable = (condition) ? value1: value2
+
+let age = 42;
+let isAdult = (age < 18) ? "Too young": "Old enough";
+console.log(isAdult);
+
+let num = 2;
+let isZero = (num == 0) ? "Zero": "Not Zero";
+console.log(isZero);
+
+let bill = 1800;
+bill = (bill >= 1500) ? bill * 0.85 : bill;
+console.log(bill);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+
+
+
+let msg = readLine();
+for (let i=0; i<3; i++) {
+    console.log(msg);
+}
