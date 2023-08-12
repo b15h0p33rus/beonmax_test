d371ec451bfc48168e9369ca5f47601395b26a84
b15h0p <b15h0p@mail.ru>
2023-08-12 09:17:32 +0300

    sample massage

--- js.js
+++ js.js
@@ -1,19 +1,4 @@
-let color = readLine();
+// alert("Я JavaScript!");
 
-/*
-    red => 1
-    green => 2
-    black => 3
-    other cases => unknown
-*/
-
-//your code goes here
-if (color == "red") {
-    console.log(1);
-} else if (color == "green") {
-    console.log(2);
-} else if (color == "black") {
-    console.log(3);
-} else {
-    console.log("unknown");
-}
+let second = 5;
+console.log(second);
